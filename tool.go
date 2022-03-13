5d59036fa706efbb6640c0ca56bd92269759c206
takoyaki-3 <mail@takoyaki3.com>
2022-03-13 11:49:28 +0900

    距離計算を追加

---
+++ tool.go
@@ -0,0 +1,52 @@
+package gogeojson
+
+import (
+	"math"
+)
+
+func (geom *Geometry)Distance()float64{
+	coordinates := geom.Coordinates.([][]float64)
+	dis := 0.0
+	for i:=0;i<len(coordinates)-1;i++{
+		from := coordinates[i]
+		to := coordinates[i+1]
+		dis += hubenyDistance(point{Lat: from[1],Lon: from[0]},point{Lat: to[1],Lon: to[0]})
+	}
+	return dis
+}
+
+// 以下、https://qiita.com/tmnck/items/30b42ba5df28c38b0f89 様の記事を一部改変の上，利用しております。
+func hubenyDistance(src, dst point) float64 {
+	dx := degree2radian(dst.Lon - src.Lon)
+	dy := degree2radian(dst.Lat - src.Lat)
+	my := degree2radian((src.Lat + dst.Lat) / 2)
+
+	W := math.Sqrt(1 - (power2(ECCENTRICITY) * power2(math.Sin(my)))) // 卯酉線曲率半径の分母
+	m_numer := EQUATORIAL_RADIUS * (1 - power2(ECCENTRICITY))         // 子午線曲率半径の分子
+
+	M := m_numer / math.Pow(W, 3) // 子午線曲率半径
+	N := EQUATORIAL_RADIUS / W    // 卯酉線曲率半径
+
+	d := math.Sqrt(power2(dy*M) + power2(dx*N*math.Cos(my)))
+
+	return d
+}
+
+func degree2radian(x float64) float64 {
+	return x * math.Pi / 180
+}
+
+func power2(x float64) float64 {
+	return math.Pow(x, 2)
+}
+
+const (
+	EQUATORIAL_RADIUS = 6378137.0            // 赤道半径 GRS80
+	POLAR_RADIUS      = 6356752.314          // 極半径 GRS80
+	ECCENTRICITY      = 0.081819191042815790 // 第一離心率 GRS80
+)
+
+type point struct {
+	Lat float64
+	Lon float64
+}
